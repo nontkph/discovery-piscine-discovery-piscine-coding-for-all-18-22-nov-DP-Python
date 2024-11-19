6f984166980e1311228dee25b69c9ab97f5fd773
MIU <101027146+nontkph@users.noreply.github.com>
2024-11-19 22:28:15 +0700

    Add files via upload

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,3 +1,15 @@
-firstNumber = input("enter first number : ")
-laastNumber = input("enter last number : ")
+firstNumber = float(input("enter first number : "))
+lastNumber = float(input("enter last number : "))
 
+result = float(firstNumber * lastNumber)
+
+print( f"{firstNumber} x {lastNumber} = {result}" )
+try:
+    if result > 0:
+        print("result is positive.")
+    elif result < 0:
+        print("result is negative.")
+    else:
+        print("what ;-;")
+except: 
+    print("error")
