37beaeba91a91b29ffae8bd932eb3f0b041aedd4
MIU <101027146+nontkph@users.noreply.github.com>
2024-11-20 02:20:21 +0700

    Add files via upload

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,16 @@
+def array_of_names(names_dict):
+    full_names = []
+    
+    for first_name, last_name in names_dict.items():
+        full_name = first_name.capitalize() + " " + last_name.capitalize()
+        full_names.append(full_name)
+    return full_names
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
